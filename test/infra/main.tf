e36d8f3529f660de2128686e9fc5b0b0485aa69e
Tom Yandell <tom@tomyandell.co>
2017-06-26 07:57:09 +0000

    Remove prefix and add bare_redirect_domain_name
    Simplify environment domains, since for sites hosted in fastly we only
    ever prepend "ENV-" for non-live environments (there is no "dev.").
    This slightly complicates the bare domain redirection, since we don't
    know the bare domain any more, so add an option for that (it makes
    sense to be optional since for some sites - e.g. admin.creditrubric.com
    - you wouldn't want the bare domain redirect since that will be saved
    for a future www.creditrubric.com).

--- test/infra/main.tf
+++ test/infra/main.tf
@@ -50,6 +50,9 @@ module "fastly_ssl_cert_hostname" {
 
 # variables
 variable "domain_name" {}
+variable "bare_redirect_domain_name" {
+  default = ""
+}
 
 variable "bare_redirect_domain_name" {
   default = ""
