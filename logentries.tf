548757481f634a7c5a70c8d86a7ecdc74dd05069
Keir Badger <keir.badger@acuris.com>
2018-07-27 12:14:20 +0100

    Remove logentries.tf and correct README

--- logentries.tf
+++
@@ -1,9 +0,0 @@
-data "logentries_logset" "fastly" {
-  name = "${var.le_logset_parent_name}"
-}
-
-resource "logentries_log" "logs" {
-  logset_id = "${data.logentries_logset.fastly.id}"
-  name      = "${var.env}-${var.domain_name}"
-  source    = "token"
-}
