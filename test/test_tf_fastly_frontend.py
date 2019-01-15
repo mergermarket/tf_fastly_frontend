26e5f38d59b3423cee30e39a98ac28c0fee0952a
Arthur Gassner <arthur.gassner@gmail.com>
2019-01-15 15:38:44 +0000

    Updates Terraform version to 0.11.11 Removes Terraform provider plugin from Dockerfile in tests Fixes tests

--- test/test_tf_fastly_frontend.py
+++ test/test_tf_fastly_frontend.py
@@ -99,6 +99,8 @@ Plan: 1 to add, 0 to change, 0 to destroy.
             'test/infra'
         ], env=self._env_for_check_output('qwerty')).decode('utf-8')
 
+        print(output)
+
         # Then
         assert re.search(template_to_re("""
         syslog.#:                                     "1"
@@ -106,7 +108,7 @@ Plan: 1 to add, 0 to change, 0 to destroy.
           """.strip()), output)  # noqa
 
         assert re.search("""
-        syslog.*.format:                      \" {  \\\\\"ddsource\\\\\": \\\\\"fastly\\\\\",  \\\\\"service\\\\\": \\\\\"%{req.http.host}V\\\\\"
+        syslog.*.format:                     \" {  \\\\\"ddsource\\\\\": \\\\\"fastly\\\\\",  \\\\\"service\\\\\": \\\\\"%{req.http.host}V\\\\\"
           """.strip(), output)  # noqa
 
         assert re.search(template_to_re("""
@@ -117,11 +119,12 @@ Plan: 1 to add, 0 to change, 0 to destroy.
           """.strip()), output)  # noqa
 
         assert re.search(template_to_re("""
-        syslog.{ident}.format_version:              "2"
-        syslog.{ident}.message_type:                "blank"
-        syslog.{ident}.name:                        "ci-www.domain.com-syslog"
-        syslog.{ident}.port:                        "10516"
-        syslog.{ident}.response_condition:          "syslog-no-shield-condition"
+        syslog.{ident}.format_version:             "2"
+        syslog.{ident}.message_type:               "blank"
+        syslog.{ident}.name:                       "ci-www.domain.com-syslog"
+        syslog.{ident}.placement:                  ""
+        syslog.{ident}.port:                       "10516"
+        syslog.{ident}.response_condition:         "syslog-no-shield-condition"
           """.strip()), output)  # noqa
 
         assert re.search(template_to_re("""
